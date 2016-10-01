9d7788d9601df7499a5918cfa9844f5c25f6a173
Jacob Hirzel <hirzel@hotmail.com>
2016-10-01 17:43:04 +0000

    Implemented search.php.

--- public/search.php
+++ public/search.php
@@ -6,6 +6,16 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
+    $zip_query = CS50::query("SELECT * FROM places WHERE postal_code LIKE ?", $_GET["geo"] . "%");
+    $places[] = $zip_query;
+    
+    $place_query = CS50::query("SELECT * FROM places WHERE place_name LIKE ?", $_GET["geo"] . "%");
+    $places[] = $place_query;
+    
+    $state_query = CS50::query("SELECT * FROM places WHERE admin_name1 LIKE ?", $_GET["geo"] . "%");
+    $places[] = $state_query;
+    
+    // print("$places[0]\n");
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
